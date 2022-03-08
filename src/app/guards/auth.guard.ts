9f6edfd159a7f4bcc820c0fcb77dea4b5487e950
josetemox <60007668+JoseTemox@users.noreply.github.com>
2022-03-07 20:42:31 -0400

    Enviroment

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanLoad, Route, UrlSegment } from '@angular/router';
+import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { UsuarioService } from '../services/usuario.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate,CanLoad {
 
   constructor(private usuarioService: UsuarioService,
               private router: Router){}
+  canLoad(route: Route, segments: UrlSegment[]): boolean | Observable<boolean> | Promise<boolean> {
+    return this.usuarioService.validartoken()
+    .pipe(
+      tap( estaAutenticado => {
+
+        if(!estaAutenticado){
+          this.router.navigateByUrl('login');
+        }
+      })
+    )
+  }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot) {
@@ -21,14 +33,14 @@ export class AuthGuard implements CanActivate {
 
     // console.log('CanActive');
     return this.usuarioService.validartoken()
-      .pipe(
-        tap( estaAutenticado => {
+    .pipe(
+      tap( estaAutenticado => {
 
-          if(!estaAutenticado){
-            this.router.navigateByUrl('login');
-          }
-        })
-      )
+        if(!estaAutenticado){
+          this.router.navigateByUrl('login');
+        }
+      })
+    )
   }
-  
+
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@
 
 export const environment = {
   production: true,
-  base_url: 'http://localhost:3000/api'
+  // base_url: 'http://localhost:3000/api'
+  base_url: 'https://adminprobackend77.herokuapp.com/api'
 };
